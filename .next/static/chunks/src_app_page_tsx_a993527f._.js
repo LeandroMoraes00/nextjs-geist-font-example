0451af423a97031824c5a9b804ef4e642b550644
LeandroMoraes00 <leandrovectra2.2@gmail.com>
2025-06-13 10:05:57 -0400

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1749823608411

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_973a4a84._.js",
+  "static/chunks/node_modules_37fac7b6._.js"
+],
+    source: "dynamic"
+});
